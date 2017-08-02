d72d15f4582e0f268ccb6030f06662e089b2e33f
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-08-02 11:31:36 +0200

    âge

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
+//EXERCICE 1
+
 var user = {
   prenom : "John",
   nom : "Rachid" ,
-  age : 45,
+  age : 15,
   pays : "France",
   paiement : ["mastercard", "visa", "electron"],
   getPropriete : function (){
     alert(this.prenom);
     alert(this.nom);
+    //alert(this.paiement[0].toUpperCase());
   },
  getAnnee : function(){
     alert(2017-this.age);
   }
+  alert(user.paiement.toUpperCase());
 }
 
 
 user.getPropriete();
 user.getAnnee();
+
+
+
+//EXERCICE 1.2
+var listes = {
+  paysAutorise : ["FRANCE", "CAMBODGE", "MARTINIQUE"],
+  moyenPaiement : ["MASTERCARD", "VISA"],
+  minAge : 34,
+
+  getAge : function (){
+    if(user.age <= listes.minAge){
+      alert("Vous n\'avez pas l'âge minimun de" + " " + this.minAge);
+    } else {
+      alert("Bienvenue");
+    }
+  }
+
+}
+
+listes.getAge();
+
+
+
+
+
+
+
+//for(x in listes) {
+//  listes[x].toUpperCase();
+//}
